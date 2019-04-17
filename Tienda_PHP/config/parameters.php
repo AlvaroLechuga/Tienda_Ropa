3b633194a4c93b13b8581933f54309b714f5060a
Álvaro Lechuga <45164539+AlvaroLechuga@users.noreply.github.com>
2019-04-18 00:40:04 +0200

    V 1.0
    Finalización del proyecto

---
+++ Tienda_PHP/config/parameters.php
@@ -0,0 +1,7 @@
+<?php
+
+define("base_url", "http://localhost/Tienda_PHP/");
+define("controller_default", "productoController");
+define("action_default", "index");
+?>
+
---
+++ Tienda_PHP/views/layout/footer.php
@@ -0,0 +1,11 @@
+</div>
+</div>
+
+
+<!-- PIE DE PÁGINA -->
+<footer id="footer">
+    <p>Desarrollado por Alvaro Lechuga &copy; <?= date('Y') ?></p>
+</footer>
+</div>
+</body>
+</html>
